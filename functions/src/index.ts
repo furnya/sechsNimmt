780663aa8f1edbc8ae8b96c9f32a832a128ace57
Lino Steinhau <furnya@gmail.com>
2020-05-11 17:57:34 +0200

    added functions

---
+++ functions/src/index.ts
@@ -0,0 +1,43 @@
+import * as functions from 'firebase-functions';
+const https = require('https');
+const options = {
+  hostname: 'sechsnimmt-33e6e.firebaseio.com',
+  port: 443,
+  path: '/card.json',
+  method: 'GET',
+};
+
+function getPromise() {
+  return new Promise((resolve, reject) => {
+    https
+      .request(options, (res: any) => {
+        let chunks_of_data: any[] = [];
+
+        res.on('data', (fragments: any) => {
+          chunks_of_data.push(fragments);
+        });
+
+        res.on('end', () => {
+          let response_body = Buffer.concat(chunks_of_data);
+          resolve(response_body.toString());
+        });
+
+        res.on('error', (error: any) => {
+          reject(error);
+        });
+      })
+      .end();
+  });
+}
+
+export const helloWorld = functions
+  .region('europe-west1')
+  .https.onRequest(async (request, response) => {
+    try {
+      let http_promise = getPromise();
+      let body = await http_promise;
+      response.set('Content-Type', 'application/json').set('Access-Control-Allow-Origin', '*').send(body);
+    } catch (error) {
+      console.log(error);
+    }
+});
--- src/app/hand/hand.component.ts
+++ src/app/hand/hand.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-hand',
@@ -9,11 +10,17 @@ export class HandComponent implements OnInit {
 
   cards = [1,2,3,4,5,6,7,8,9,10];
 
-  selectedCard = 2;
+  selectedCard = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get(
+        'https://europe-west1-sechsnimmt-33e6e.cloudfunctions.net/helloWorld'
+      ).subscribe((data: string) => {
+      console.log(data);
+      this.selectedCard = +data;
+    });
   }
 
   onCardSelected(id: number) {
