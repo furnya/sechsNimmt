61d116032ce8c34a5052680a1fbbfe962efe7c9a
Lino Steinhau <lino.steinhau@fu-berlin.de>
2021-01-17 01:43:59 +0100

    refactoring

--- src/app/game/board/board.component.ts
+++ src/app/game/board/board.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit, OnDestroy, HostListener, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  HostListener,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { GameService, NO_CARD_SELECTED } from '../game.service';
+import {
+  GameService,
+  NO_CARD_SELECTED,
+  NO_PLAYER_INDEX,
+} from '../game.service';
 import { Subscription } from 'rxjs';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { FinishedDialogComponent } from 'src/app/game/board/finished-dialog/finished-dialog.component';
 import { GLOBAL_CONFIG } from 'src/app/config/global-config';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MinusPointsComponent } from './minus-points/minus-points.component';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 import { DocumentService } from 'src/app/utils/document.service';
 import { timeout } from 'rxjs/operators';
+import { GameState } from 'src/app/models/game.model';
 
 const YOUR_TURN_PUT_HINT = 'Du bist am Zug. Lege deine Karte an!';
 const YOUR_TURN_TAKE_HINT = 'Du bist am Zug. Wähle eine Reihe aus!';
@@ -23,18 +40,20 @@ const CHOOSE_HINT = 'Wähle eine Karte aus!';
   animations: [
     trigger('showHideHint', [
       // ...
-      state('open', style({
-        top: '0'
-      })),
-      state('closed', style({
-        top: '-10vh'
-      })),
-      transition('open => closed', [
-        animate('0.5s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
+      state(
+        'open',
+        style({
+          top: '0',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          top: '-10vh',
+        })
+      ),
+      transition('open => closed', [animate('0.5s')]),
+      transition('closed => open', [animate('0.5s')]),
     ]),
   ],
 })
@@ -49,7 +68,13 @@ export class BoardComponent implements OnInit, OnDestroy {
   timeoutInterval;
   timeLeft = 0;
   get timeLeftString() {
-    return Math.floor(this.timeLeft / 60) + ':' + (this.timeLeft % 60 < 10 ? '0' + this.timeLeft % 60 : this.timeLeft % 60);
+    return (
+      Math.floor(this.timeLeft / 60) +
+      ':' +
+      (this.timeLeft % 60 < 10
+        ? '0' + (this.timeLeft % 60)
+        : this.timeLeft % 60)
+    );
   }
   shouldSelectRandomCard = false;
 
@@ -75,75 +100,109 @@ export class BoardComponent implements OnInit, OnDestroy {
     this.gameService.startGame(this.route.snapshot.params.id);
     this.gameStateSub = this.gameService.gameStateChanged.subscribe(
       (gameState) => {
-        if (this.gameService.playerIndex !== -1 && gameState?.finished) {
-          if (this.dialogOpen) {
-            this.dialogRef.componentInstance.data = gameState;
-          } else {
-            this.dialogOpen = true;
-            this.dialogRef = this.finishedGameDialog.open(
-              FinishedDialogComponent,
-              {
-                data: gameState,
-                disableClose: true,
-                panelClass: 'finished-dialog'
-              }
-            );
-            this.dialogRef.afterClosed().subscribe((result) => {
-              this.dialogOpen = false;
-              this.dialogRef = null;
-            });
-          }
+        if (
+          this.gameService.playerIndex !== NO_PLAYER_INDEX &&
+          gameState?.finished
+        ) {
+          this.gameIsFinished(gameState);
         } else {
           this.finishedGameDialog.closeAll();
         }
-        if (gameState.choosingCards && this.gameService.getSelectedCard() === 0) {
-          this.hintOpen = true;
-          this.hint = CHOOSE_HINT;
-          if (this.gameService.selectedCardLocally !== 0) {
-            this.gameService.selectCard(this.gameService.selectedCardLocally);
-          } else if (!this.timeout && !this.gameService.options.thinkingTimeoutDisabled.value) {
-            this.showTimeout = true;
-            this.timeLeft = this.gameService.options.maxThinkingTime.value;
-            this.timeoutInterval = setInterval(() => {
-              this.timeLeft--;
-            }, 1000);
-            this.timeout = setTimeout(() => {
-              this.shouldSelectRandomCard = true;
-              this.showTimeout = false;
-              clearInterval(this.timeoutInterval);
-              const cardIndex = Math.floor(Math.random() * this.gameService.getHandCards().length);
-              this.gameService.selectCard(this.gameService.getHandCards()[cardIndex]).then(() => {
-                this.shouldSelectRandomCard = true;
-              });
-            }, this.gameService.options.maxThinkingTime.value * 1000);
-          }
+        if (
+          gameState.choosingCards &&
+          this.gameService.getSelectedCard() === NO_CARD_SELECTED
+        ) {
+          this.choosingAndNoCardSelected();
         } else {
           if (!this.gameService.isChoosingCards()) {
             this.gameService.selectedCardLocally = NO_CARD_SELECTED;
           }
           if (!this.gameService.options.thinkingTimeoutDisabled.value) {
-            this.shouldSelectRandomCard = false;
-            this.showTimeout = false;
-            clearInterval(this.timeoutInterval);
-            clearTimeout(this.timeout);
-            this.timeout = null;
+            this.stopTimeout();
           }
-          if (this.gameService.isYourTurn()) {
-            this.hintOpen = true;
-            if (this.canTakeRow()) {
-              this.hint = YOUR_TURN_TAKE_HINT;
-            } else {
-              this.hint = YOUR_TURN_PUT_HINT;
-            }
-          } else if (!gameState.choosingCards && this.gameService.getTurnPlayerName()) {
-            this.hintOpen = true;
-            this.hint = this.gameService.getTurnPlayerName() + NOT_YOUR_TURN_HINT;
-          } else {
-            this.hintOpen = false;
-          }
+          this.setTurnHint(gameState);
         }
       }
     );
+  }
+
+  setTurnHint(gameState: GameState) {
+    if (this.gameService.isYourTurn()) {
+      this.hintOpen = true;
+      if (this.canTakeRow()) {
+        this.hint = YOUR_TURN_TAKE_HINT;
+      } else {
+        this.hint = YOUR_TURN_PUT_HINT;
+      }
+    } else if (
+      !gameState.choosingCards &&
+      this.gameService.getTurnPlayerName()
+    ) {
+      this.hintOpen = true;
+      this.hint =
+        this.gameService.getTurnPlayerName() + NOT_YOUR_TURN_HINT;
+    } else {
+      this.hintOpen = false;
+    }
+  }
+
+  stopTimeout() {
+    this.shouldSelectRandomCard = false;
+    this.showTimeout = false;
+    clearInterval(this.timeoutInterval);
+    clearTimeout(this.timeout);
+    this.timeout = null;
+  }
+
+  startTimeout() {
+    this.showTimeout = true;
+    this.timeLeft = this.gameService.options.maxThinkingTime.value;
+    this.timeoutInterval = setInterval(() => {
+      this.timeLeft--;
+    }, 1000);
+    this.timeout = setTimeout(() => {
+      this.shouldSelectRandomCard = true;
+      this.showTimeout = false;
+      clearInterval(this.timeoutInterval);
+      const cardIndex = Math.floor(
+        Math.random() * this.gameService.getHandCards().length
+      );
+      this.gameService
+        .selectCard(this.gameService.getHandCards()[cardIndex])
+        .then(() => {
+          this.shouldSelectRandomCard = true;
+        });
+    }, this.gameService.options.maxThinkingTime.value * 1000);
+  }
+
+  choosingAndNoCardSelected() {
+    this.hintOpen = true;
+    this.hint = CHOOSE_HINT;
+    if (this.gameService.selectedCardLocally !== NO_CARD_SELECTED) {
+      this.gameService.selectCard(this.gameService.selectedCardLocally);
+    } else if (
+      !this.timeout &&
+      !this.gameService.options.thinkingTimeoutDisabled.value
+    ) {
+      this.startTimeout();
+    }
+  }
+
+  gameIsFinished(gameState: GameState) {
+    if (this.dialogOpen) {
+      this.dialogRef.componentInstance.data = gameState;
+    } else {
+      this.dialogOpen = true;
+      this.dialogRef = this.finishedGameDialog.open(FinishedDialogComponent, {
+        data: gameState,
+        disableClose: true,
+        panelClass: 'finished-dialog',
+      });
+      this.dialogRef.afterClosed().subscribe((result) => {
+        this.dialogOpen = false;
+        this.dialogRef = null;
+      });
+    }
   }
 
   canTakeRow(): boolean {
@@ -162,9 +221,8 @@ export class BoardComponent implements OnInit, OnDestroy {
   }
 
   openAllMinusPoints() {
-    this.minusPointsBottomSheet.open(MinusPointsComponent,
-      {
-        panelClass: 'minus-points-sheet-container'
-      });
+    this.minusPointsBottomSheet.open(MinusPointsComponent, {
+      panelClass: 'minus-points-sheet-container',
+    });
   }
 }
