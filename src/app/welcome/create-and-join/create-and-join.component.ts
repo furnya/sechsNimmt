2678b4f2de5db2469c08de4bc5206e2377c93cdf
Lino Steinhau <lino.steinhau@fu-berlin.de>
2021-01-30 13:19:09 +0100

    small change in welcome screen

--- src/app/welcome/create-and-join/create-and-join.component.ts
+++ src/app/welcome/create-and-join/create-and-join.component.ts
@@ -4,15 +4,14 @@ import {
   Component,
   OnDestroy,
   OnInit,
-  ViewChild,
+  ViewChild
 } from '@angular/core';
-import { FormControl, NgForm } from '@angular/forms';
+import { FormControl, NgForm, NgModel } from '@angular/forms';
+import { MatIconRegistry } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Subscription } from 'rxjs';
 import { RoomCreationService } from '../room-creation.service';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-and-join',
@@ -21,17 +20,14 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class CreateAndJoinComponent
   implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild('joinGameIdFormControl') joinGameIdFormControl: FormControl;
+  @ViewChild('joinGameIdFormControl') joinGameIdModel: NgModel;
   queuedRoomIds: string[] = [];
-  // filteredOptions: Observable<string[]>;
-  filteredRoomIds: string[];
   queuedRoomsSubscription: Subscription;
 
   constructor(
     private roomCreationService: RoomCreationService,
     private errorSnackBar: MatSnackBar,
     private cdr: ChangeDetectorRef,
-    private router: Router,
     private iconRegistry: MatIconRegistry,
     private sanitizer: DomSanitizer
   ) {
@@ -45,40 +41,20 @@ export class CreateAndJoinComponent
 
   ngOnInit(): void {
     this.roomCreationService.clearRoom();
-    this.setRoomIds(this.roomCreationService.queuedRoomIds);
     this.queuedRoomsSubscription = this.roomCreationService.queuedRoomsChanged.subscribe(
       (roomIds) => {
         this.setRoomIds(roomIds);
-        this.filteredRoomIds = this.getFilteredOptions();
       }
     );
   }
 
   ngAfterViewInit() {
-    this.filteredRoomIds = this.getFilteredOptions();
-    this.cdr.detectChanges();
+    this.setRoomIds(this.roomCreationService.queuedRoomIds);
   }
 
   ngOnDestroy() {
     this.queuedRoomsSubscription.unsubscribe();
   }
-
-  private getFilteredOptions(): string[] {
-    return this.queuedRoomIds;
-  }
-  // private getFilteredOptions(): Observable<string[]> {
-  //   return this.joinGameIdFormControl.valueChanges.pipe(
-  //     startWith(''),
-  //     map((value) => this._filter(this.queuedGameIds, value))
-  //   );
-  // }
-
-  // private _filter(options, value: string): string[] {
-  //   const filterValue = value?.toLowerCase();
-  //   return options.filter((option) => {
-  //     return option.toLowerCase().includes(filterValue);
-  //   });
-  // }
 
   setRoomIds(roomIds: string[]) {
     this.queuedRoomIds = [];
@@ -87,20 +63,14 @@ export class CreateAndJoinComponent
         this.queuedRoomIds.push(roomId);
       }
     });
+    if (!this.joinGameIdModel.control.value && this.queuedRoomIds?.length > 0) {
+      this.joinGameIdModel.control.setValue(this.queuedRoomIds[0]);
+      this.cdr.detectChanges();
+    }
   }
 
   onJoinRoom(joinRoomForm: NgForm) {
     const roomId = joinRoomForm.value.joinGameId;
-    // if (!this.queuedRoomIds.includes(roomId)) {
-    //   this.errorSnackBar.open(
-    //     'Ein Spiel mit dieser ID existiert nicht!',
-    //     null,
-    //     {
-    //       duration: 3000,
-    //     }
-    //   );
-    //   return;
-    // }
     const error = this.roomCreationService.joinRoomFromForm(
       roomId,
       joinRoomForm.value.playerName,
