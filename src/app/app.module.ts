c26bca1ec8ed8b39f881ed623bf9a3628c4d6633
Lino Steinhau <furnya@gmail.com>
2021-01-31 23:46:52 +0100

    update prep 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ENVIRONMENT } from './config/global-config';
 import { NotFoundComponent } from './error-handling/not-found/not-found.component';
-import { FinishedDialogComponent } from './game/board/finished-dialog/finished-dialog.component';
 import { GameModule } from './game/game.module';
 import { AppMaterialModule } from './modules/app-material.module';
-import { EnterNameDialogComponent } from './welcome/enter-name-dialog/enter-name-dialog.component';
 import { WelcomeModule } from './welcome/welcome.module';
 
 @NgModule({
